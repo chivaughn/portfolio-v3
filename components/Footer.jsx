153f853c0b99c5c611c6308c98b28d77896a2351
Chivaughn Charles <chivaughn.charles@my.uwi.edu>
2022-07-19 00:22:46 -0400

    fixed footer hover

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
       <span className="block text-sm text-gray-500 text-center dark:text-gray-400">
         © 2022 Built with 🏳️‍🌈 by
-        <span className="hover:bg-blue-500 text-gray-700 dark:text-white  rounded-md px-2 py-1  inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
+        <span className="hover:bg-blue-500 hover:text-white text-gray-700 dark:text-white  rounded-md px-2 py-1  inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
           chivaughn
         </span>
       </span>
