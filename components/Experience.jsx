36057eebdff5327ee726147d38de877628ef4722
chivaughn <chivaughn.charles@my.uwi.edu>
2026-01-14 18:21:15 -0400

    Update experience section with new job details and responsibilities

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -12,16 +12,24 @@ export const Experience = () => {
       <div className="grid grid-cols-1  max-w-xl mx-auto pt-20">
         <div className="relative experience-card p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4 dark:shadow-lg dark:shadow-gray-700">
           <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
-            2022
+            2024
           </h1>
           <h1 className="font-semibold text-xl dark:text-gray-300">
-            Freelance Software Developer
+            Agostini Limited - Innovation Lab
           </h1>
-          <a href="https://chytes.com" className="text-gray-500">
-            Chytes Software
+          <a href="https://agostinilimited.com/" className="text-gray-500">
+            Agostini Limited - Innovation Lab
           </a>
           <p className="text-gray-600 dark:text-gray-400 my-2">
-            Founded a software development company.
+            Currently working as a Front-End Developer at Agostini Limited's
+            Innovation Lab. Responsible for designing and implementing user
+            interfaces for various web applications that support the company's
+            digital transformation initiatives. Collaborating with
+            cross-functional teams to create innovative solutions that enhance
+            customer experiences and drive business growth. Utilizing
+            technologies such as Flutter, Svelte and Firebase to deliver
+            high-quality applications that meet the needs of both internal and
+            external stakeholders.
           </p>
         </div>
         <div className="divider-container flex flex-col items-center -mt-2">
@@ -30,67 +38,112 @@ export const Experience = () => {
           </div>
           <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
         </div>
+        <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
+          2023
+        </h1>
+        <h1 className="font-semibold text-xl dark:text-gray-300">
+          Nucleus Automations Systems
+        </h1>
+        <a href="https://www.nucleusltd.com/" className="text-gray-500">
+          Nucleus Automations Systems
+        </a>
+        <p className="text-gray-600 dark:text-gray-400 my-2">
+          Worked as a Full-Stack Developer for a major automation company in
+          Trinidad. Developed and maintained web applications that streamlined
+          internal processes and improved overall efficiency. Collaborated with
+          cross-functional teams to design and implement new features, ensuring
+          seamless integration with existing systems. Utilized technologies such
+          as PHP, Laravel, JavaScript, and MySQL to deliver high-quality
+          solutions that met business requirements.
+        </p>
+      </div>
+      <div className="divider-container flex flex-col items-center -mt-2">
+        <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 ">
+          <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
+        </div>
+        <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+      </div>
+      <div className="relative experience-card p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4 dark:shadow-lg dark:shadow-gray-700">
+        <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
+          2022
+        </h1>
+        <h1 className="font-semibold text-xl dark:text-gray-300">
+          HARTT - Humanitarian Association of the Republic of T & T
+        </h1>
+        <a href="https://thehartt.com/" className="text-gray-500">
+          HARTT
+        </a>
+        <p className="text-gray-600 dark:text-gray-400 my-2">
+          Joined a NPO as a Front-End Developer to help build and maintain their
+          website and web applications. Worked with a team to develop
+          user-friendly interfaces and ensure seamless user experiences across
+          all platforms. Worked on a project to connect rural communities to
+          essential trafic information using Flutter for the front-end and
+          Firebase for the backend.
+        </p>
+      </div>
+      <div className="divider-container flex flex-col items-center -mt-2">
+        <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 ">
+          <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
+        </div>
+        <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+      </div>
 
-        <div className="relative experience-card  p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4 dark:shadow-lg dark:shadow-gray-700">
-          <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
-            2020
-          </h1>
-          <h1 className="font-semibold text-xl dark:text-gray-300">
-            Full Stack Developer
-          </h1>
-          <a href="https://cenedex.com" className="text-gray-500">
-            Cenedex Soltions
-          </a>
-          <p className="text-gray-600 dark:text-gray-400 my-2">
-            I was tasked with developing a management system for a magento site.
-            The administrative panel was built using Laravel. Optimization was a
-            bit difficult seeing as magento is a hefty system when there is a
-            lot of data but that was worked around with better queries and the
-            reduction of redundant code.
-          </p>
+      <div className="relative experience-card  p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4 dark:shadow-lg dark:shadow-gray-700">
+        <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
+          2020
+        </h1>
+        <h1 className="font-semibold text-xl dark:text-gray-300">
+          Full Stack Developer
+        </h1>
+        <a href="https://cenedex.com" className="text-gray-500">
+          Cenedex Soltions
+        </a>
+        <p className="text-gray-600 dark:text-gray-400 my-2">
+          I was tasked with developing a management system for a magento site.
+          The administrative panel was built using Laravel. Optimization was a
+          bit difficult seeing as magento is a hefty system when there is a lot
+          of data but that was worked around with better queries and the
+          reduction of redundant code.
+        </p>
+      </div>
+      <div className="divider-container flex flex-col items-center -mt-2">
+        <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
+          <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
         </div>
-        <div className="divider-container flex flex-col items-center -mt-2">
-          <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
-            <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
-          </div>
-          <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+        <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+      </div>
+      <div className="relative experience-card  p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4 dark:shadow-lg dark:shadow-gray-700">
+        <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
+          2019
+        </h1>
+        <h1 className="font-semibold text-xl dark:text-gray-300">Internship</h1>
+        <a href="https://cenedex.com" className="text-gray-500">
+          Cendex Solutions
+        </a>
+        <p className="text-gray-600 dark:text-gray-400 my-2">
+          I developed a database management system using React.js, a framework
+          that was used to add, modify and delete data entries which were used
+          to manage the backend of a Shop assistant Artificial Intelligence.
+        </p>
+      </div>
+      <div className="divider-container flex flex-col items-center -mt-2">
+        <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
+          <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
         </div>
-        <div className="relative experience-card  p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4 dark:shadow-lg dark:shadow-gray-700">
-          <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
-            2019
-          </h1>
-          <h1 className="font-semibold text-xl dark:text-gray-300">
-            Internship
-          </h1>
-          <a href="https://cenedex.com" className="text-gray-500">
-            Cendex Solutions
-          </a>
-          <p className="text-gray-600 dark:text-gray-400 my-2">
-            I developed a database management system using React.js, a framework
-            that was used to add, modify and delete data entries which were used
-            to manage the backend of a Shop assistant Artificial Intelligence.
-          </p>
-        </div>
-        <div className="divider-container flex flex-col items-center -mt-2">
-          <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
-            <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
-          </div>
-          <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
-        </div>
-        <div className="relative experience-card  p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4 dark:shadow-lg dark:shadow-gray-700">
-          <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
-            2019
-          </h1>
-          <h1 className="font-semibold text-xl dark:text-gray-300">
-            Graduation
-          </h1>
-          <a href="https://sta.uwi.edu/" className="text-gray-500">
-            UWI, St Augustine.
-          </a>
-          <p className="text-gray-600 dark:text-gray-400 my-2">
-            Major in Computer Science and Mathematics
-          </p>
-        </div>
+        <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+      </div>
+      <div className="relative experience-card  p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4 dark:shadow-lg dark:shadow-gray-700">
+        <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-300 font-bold dark:text-gray-700">
+          2019
+        </h1>
+        <h1 className="font-semibold text-xl dark:text-gray-300">Graduation</h1>
+        <a href="https://sta.uwi.edu/" className="text-gray-500">
+          UWI, St Augustine.
+        </a>
+        <p className="text-gray-600 dark:text-gray-400 my-2">
+          Major in Computer Science and Mathematics
+        </p>
       </div>
     </div>
   );
